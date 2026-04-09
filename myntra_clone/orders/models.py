f51f37980939c033c1c1196b2569eefac5c96a40
ibbani <ibbanigowda98@gmail.com>
2026-04-09 17:06:03 +0530

    myntra : initial commit

---
+++ myntra_clone/orders/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_price = models.FloatField()
+    status = models.CharField(max_length=50, default='pending')
