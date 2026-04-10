4af00d349bde1f387a336297bb4f8a9edfee8ed8
ibbani <ibbanigowda98@gmail.com>
2026-04-10 17:07:03 +0530

    myntra:initial commit

--- myntra_clone/cart/views.py
+++ myntra_clone/cart/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Cart
+from .serializers import CartSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class CartViewSet(viewsets.ModelViewSet):
+    serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Cart.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        print(self.request.user,"-----------------------")
+        serializer.save(user=self.request.user)
